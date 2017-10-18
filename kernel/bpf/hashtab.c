b7cbb51edbb8d03c541f4e506434610c1414791b
Chenbo Feng <fengc@google.com>
2017-10-18 13:00:22 -0700

    BACKPORT: bpf: Add file mode configuration into bpf maps
    Introduce the map read/write flags to the eBPF syscalls that returns the
    map fd. The flags is used to set up the file mode when construct a new
    file descriptor for bpf maps. To not break the backward capability, the
    f_flags is set to O_RDWR if the flag passed by syscall is 0. Otherwise
    it should be O_RDONLY or O_WRONLY. When the userspace want to modify or
    read the map content, it will check the file mode to see if it is
    allowed to make the change.
    
    Signed-off-by: Chenbo Feng <fengc@google.com>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Deleted the file mode configuration code in unsupported map type and
    removed the file mode check in non-existing helper functions.
    (cherry-pick from net-next: 6e71b04a82248ccf13a94b85cbc674a9fefe53f5)
    Bug: 30950746
    
    Change-Id: Icfad20f1abb77f91068d244fb0d87fa40824dd1b

--- kernel/bpf/hashtab.c
+++ kernel/bpf/hashtab.c
@@ -15,6 +15,8 @@
 #include <linux/filter.h>
 #include <linux/vmalloc.h>
 #include "percpu_freelist.h"
+#define HTAB_CREATE_FLAG_MASK						\
+	(BPF_F_NO_PREALLOC | BPF_F_RDONLY | BPF_F_WRONLY)
 
 struct bucket {
 	struct hlist_head head;
@@ -121,7 +123,7 @@ static struct bpf_map *htab_map_alloc(union bpf_attr *attr)
 	int err, i;
 	u64 cost;
 
-	if (attr->map_flags & ~BPF_F_NO_PREALLOC)
+	if (attr->map_flags & ~HTAB_CREATE_FLAG_MASK)
 		/* reserved bits should not be used */
 		return ERR_PTR(-EINVAL);
 
